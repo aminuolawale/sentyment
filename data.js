355a7b77370294920417c838be131f5e69123ad8
mohammedaminu <ammodadeveloper@gmail.com>
2025-05-10 00:53:57 +0200

    Inital commit

---
+++ data.js
@@ -0,0 +1,123 @@
+export const data = [
+    {
+      governor: "Alex Otti",
+      state: "Abia",
+      years: "2023–present",
+      sentiment: "Positive",
+      link: "https://alexotti.com/manifesto",
+      wiki: "https://en.wikipedia.org/wiki/Alex_Otti"
+    },
+    {
+      governor: "Ahmadu Umaru Fintiri",
+      state: "Adamawa",
+      years: "2019–present",
+      sentiment: "Mixed to Positive",
+      link: "https://adspc.ad.gov.ng/state-projects/?utm",
+      wiki: "https://en.wikipedia.org/wiki/Ahmadu_Umaru_Fintiri"
+    },
+    {
+      governor: "Umo Eno",
+      state: "Akwa Ibom",
+      years: "2023–present",
+      sentiment: "Positive",
+      link: "https://umoeno.com/manifesto",
+      wiki: "https://en.wikipedia.org/wiki/Umo_Eno"
+    },
+    {
+      governor: "Charles Soludo",
+      state: "Anambra",
+      years: "2022–present",
+      sentiment: "Positive",
+      link: "https://anambrastate.gov.ng/manifesto",
+      wiki: "https://en.wikipedia.org/wiki/Charles_Chukwuma_Soludo"
+    },
+    {
+      governor: "Bala Mohammed",
+      state: "Bauchi",
+      years: "2019–present",
+      sentiment: "Positive",
+      link: "https://balamohammed.com/manifesto",
+      wiki: "https://en.wikipedia.org/wiki/Bala_Mohammed"
+    },
+    {
+      governor: "Douye Diri",
+      state: "Bayelsa",
+      years: "2020–present",
+      sentiment: "Positive",
+      link: "https://douyediri.com/manifesto",
+      wiki: "https://en.wikipedia.org/wiki/Douye_Diri"
+    },
+    {
+      governor: "Hyacinth Alia",
+      state: "Benue",
+      years: "2023–present",
+      sentiment: "Positive",
+      link: "https://hyacinthalia.com/manifesto",
+      wiki: "https://en.wikipedia.org/wiki/Hyacinth_Alia"
+    },
+    {
+      governor: "Babagana Zulum",
+      state: "Borno",
+      years: "2019–present",
+      sentiment: "Positive",
+      link: "https://borno.gov.ng/manifesto",
+      wiki: "https://en.wikipedia.org/wiki/Babagana_Umar_Zulum"
+    },
+    {
+      governor: "Bassey Otu",
+      state: "Cross River",
+      years: "2023–present",
+      sentiment: "Positive",
+      link: "https://basseyotu.com/manifesto",
+      wiki: "https://en.wikipedia.org/wiki/Bassey_Otu"
+    },
+    {
+      governor: "Sheriff Oborevwori",
+      state: "Delta",
+      years: "2023–present",
+      sentiment: "Positive",
+      link: "https://sheriffoborevwori.com/manifesto",
+      wiki: "https://en.wikipedia.org/wiki/Sheriff_Oborevwori"
+    },
+    {
+      governor: "Francis Nwifuru",
+      state: "Ebonyi",
+      years: "2023–present",
+      sentiment: "Positive",
+      link: "https://francisnwifuru.com/manifesto",
+      wiki: "https://en.wikipedia.org/wiki/Francis_Nwifuru"
+    },
+    {
+      governor: "Godwin Obaseki",
+      state: "Edo",
+      years: "2016–2024",
+      sentiment: "Positive",
+      link: "https://edostate.gov.ng/manifesto",
+      wiki: "https://en.wikipedia.org/wiki/Godwin_Obaseki"
+    },
+    {
+      governor: "Biodun Oyebanji",
+      state: "Ekiti",
+      years: "2022–present",
+      sentiment: "Positive",
+      link: "https://biodunoyebanji.com/manifesto",
+      wiki: "https://en.wikipedia.org/wiki/Biodun_Oyebanji"
+    },
+    {
+      governor: "Peter Mbah",
+      state: "Enugu",
+      years: "2023–present",
+      sentiment: "Positive",
+      link: "https://petermbah.com/manifesto",
+      wiki: "https://en.wikipedia.org/wiki/Peter_Mbah"
+    },
+    {
+      governor: "Muhammad Inuwa Yahaya",
+      state: "Gombe",
+      years: "2019–present",
+      sentiment: "Positive",
+      link: "https://inuwayahaya.com/manifesto",
+      wiki: "https://en.wikipedia.org/wiki/Muhammadu_Inuwa_Yahaya"
+    }
+  ];
+module.exports = data
